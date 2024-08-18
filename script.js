158ae97ff94b4761ebafda6498d9320f1e0e0180
Zainul khan <zainulkhan032@gmail.com>
2024-08-18 16:53:05 +0530

    add footer and header

--- script.js
+++ script.js
@@ -50,11 +50,11 @@ async function loginCeck() {
   let a = await fetch("cara/login/login.json");
   let response = await a.text();
   const data = JSON.parse(response);
-  let loginDiv = document.querySelector(".navbar .login");
-  let profileDiv = document.querySelector(".navbar .profile");
+  let loginDiv = document.querySelector("#login");
+  let profileDiv = document.querySelector("#header .navbar .profile");
   let profileName = document.getElementById("profile-name");
   let index = localStorage.getItem("index");
-
+  console.log(loginDiv)
   if (index != null) {
     loginDiv.style.display = "none";
     profileDiv.style.display = "flex";
